9e565b04c5b32332c6133db2871e13d3185521f1
HumbertoMartinez <9hmartinez@stpatrick.org>
2017-02-02 13:53:25 -0600

    Done

---
+++ degree.js
@@ -0,0 +1,29 @@
+const Matrix = require("matrix-js");
+
+let A = Matrix([
+    [1,1,0,1,0],
+    [1,0,1,0,0],
+    [0,1,0,0,0],
+    [1,0,0,0,1],
+    [0,0,0,1,0]
+]);
+//Code here
+function degree(mtrx,v){
+  var sum = 0;
+  var degreelist = mtrx(v);
+  for(let d=0;d<degreelist.length;d++){
+    sum = sum+degreelist[d];
+  }
+  return sum;
+}
+
+function degreecentrality(mtrx,v){
+let neighbors = mtrx(v).length;
+return degree(mtrx,v)/(neighbors-1);
+}
+
+//Challenge
+
+//Run test code with ctrl+shift+p  then type in "script"
+console.log(degree(A,3));
+console.log(degreecentrality(A,2));
